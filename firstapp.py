3764b41c87b1e7c651414ee9cf54885eaaeae0bf
ichisakiln <releaseanimates@gmail.com>
2024-08-06 20:05:52 +0900

    firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('HAPPY BIRTHDAY')
-st.write('THATS CRAZY ITS YOUR BIRTHDAY (I dont care if it is or not. IT IS NOW YOUR BIRTHDAY WOOHOOOOOOOO')
+st.write('THATS CRAZY ITS YOUR BIRTHDAY (I dont care if it is or not. IT IS NOW YOUR BIRTHDAY WOOHOOOOOOOO mess around with the slider for a surprise')
 slider_value = st.slider('Select a value', min_value=0,max_value=100, value=50)
 if (slider_value > 51):
     VIDEO_URL = "https://www.youtube.com/watch?v=NT3zp2vYMvo"
